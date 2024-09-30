6789c8b9e791c112bb8b995d09cd5c843a78d5e3
Jisung Lee <jskurt9352@gmail.com>
2024-09-30 17:16:06 +0900

    feat: add the add feature to todo-list

--- src/components/todo-list/editor/editor.jsx
+++ src/components/todo-list/editor/editor.jsx
@@ -1,10 +1,38 @@
+import { useRef, useState } from 'react'
 import './editor.css'
 
-const Editor = () => {
+const Editor = ({ handleToSetTodos }) => {
+  const [content, setContent] = useState('')
+  const contentRef = useRef()
+  const handleToSetTodo = (e) => {
+    setContent(e.target.value)
+  }
+
+  const handleToSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus()
+      return
+    }
+    handleToSetTodos(content)
+    setContent('')
+  }
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      handleToSubmit()
+    }
+  }
+
   return (
     <div className="editor">
-      <input placeholder="new Todo..." />
-      <button>Add</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={handleToSetTodo}
+        placeholder="new Todo..."
+      />
+      <button onClick={handleToSubmit}>Add</button>
     </div>
   )
 }
--- src/components/todo-list/todo-list/todo-list.jsx
+++ src/components/todo-list/todo-list/todo-list.jsx
@@ -1,13 +1,27 @@
+import { useRef, useState } from 'react'
 import Editor from '../editor/editor'
 import Header from '../header/header'
 import List from '../list/list'
 import './todo-list.css'
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([])
+  const idRef = useRef(0)
+  const handleToSetTodos = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      content: content,
+      date: new Date().getTime(),
+      isChecked: false
+    }
+
+    setTodos([newTodo, ...todos])
+  }
+
   return (
     <div className="todo-list">
       <Header />
-      <Editor />
+      <Editor handleToSetTodos={handleToSetTodos} />
       <List />
     </div>
   )
