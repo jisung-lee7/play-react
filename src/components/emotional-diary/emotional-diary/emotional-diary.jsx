077d812dc559f8591ab7e9c58cd795c61960ff92
Jisung Lee <jskurt9352@gmail.com>
2024-10-08 08:06:25 +0900

    feat: add button and header of elements to emotional-diary

--- src/components/emotional-diary/emotional-diary/emotional-diary.jsx
+++ src/components/emotional-diary/emotional-diary/emotional-diary.jsx
@@ -1,31 +1,13 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import Diary from '../pages/diary'
 import Home from '../pages/home'
 import New from '../pages/new'
 import Notfound from '../pages/notfound'
 import Edit from '../pages/edit'
-import { getEmotionImage } from '../../../utils/get-emotion-image'
+import { Route, Routes } from 'react-router-dom'
 
 const EmotionalDiary = () => {
-  const nav = useNavigate()
-  const handleToClick = () => {
-    nav('/new')
-  }
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={handleToClick}>Navigate to a new page</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
