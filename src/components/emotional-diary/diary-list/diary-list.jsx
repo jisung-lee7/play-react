b41d17d17ba59822ac99fa3a36da4c1aa1dfedb3
Jisung Lee <jskurt9352@gmail.com>
2024-10-09 16:59:49 +0900

    feat: add navigate to New page and sort diarys

--- src/components/emotional-diary/diary-list/diary-list.jsx
+++ src/components/emotional-diary/diary-list/diary-list.jsx
@@ -1,19 +1,43 @@
 import './diary-list.css'
 import DiaryItem from '../diary-item/diary-item'
 import Button from '../elements/button/button'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const DiaryList = ({ filteredDiarys }) => {
+  const navigate = useNavigate()
+  const [sortType, setSortType] = useState('latest')
+
+  const handleToSortType = (e) => {
+    setSortType(e.target.value)
+  }
+
+  const getSortedDiarys = () => {
+    return filteredDiarys.toSorted((a, b) => {
+      if (sortType === 'oldest') {
+        return Number(a.createdDate) - Number(b.createdDate)
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate)
+      }
+    })
+  }
+
+  const sortedDiarys = getSortedDiarys()
   return (
     <div className="diary-list">
       <div className="menu_bar">
-        <select>
+        <select onChange={handleToSortType}>
           <option value={'latest'}>Latest</option>
           <option value={'oldest'}>Oldest</option>
         </select>
-        <Button text={'Create diary'} type={'POSITIVE'} />
+        <Button
+          text={'Create diary'}
+          type={'POSITIVE'}
+          onClick={() => navigate('/new')}
+        />
       </div>
       <div className="list-wrapper">
-        {filteredDiarys.map((diary) => {
+        {sortedDiarys.map((diary) => {
           return (
             <DiaryItem
               key={diary.id}
