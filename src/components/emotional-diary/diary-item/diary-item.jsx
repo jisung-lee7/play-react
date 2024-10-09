a5aff4655260521eaf49f8498fdd09fdee54874a
Jisung Lee <jskurt9352@gmail.com>
2024-10-09 16:43:17 +0900

    feat: add navigate feature to DiaryItem

--- src/components/emotional-diary/diary-item/diary-item.jsx
+++ src/components/emotional-diary/diary-item/diary-item.jsx
@@ -1,21 +1,26 @@
 import './diary-item.css'
 import { getEmotionImage } from '../../../utils/get-emotion-image'
 import Button from '../elements/button/button'
+import { useNavigate } from 'react-router-dom'
 
 const DiaryItem = ({ id, createdDate, emotionId, content }) => {
+  const navigate = useNavigate()
   return (
     <div className="diary-item">
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        className={`img_section img_section_${emotionId}`}
+        onClick={() => navigate(`/diary/${id}`)}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className="info_section">
+      <div className="info_section" onClick={() => navigate(`/diary/${id}`)}>
         <div className="created_date">
           {new Date(createdDate).toLocaleDateString()}
         </div>
         <div className="content">{content}</div>
       </div>
       <div className="button_section">
-        <Button text={'edit'} />
+        <Button onClick={() => navigate(`/edit/${id}`)} text={'edit'} />
       </div>
     </div>
   )
