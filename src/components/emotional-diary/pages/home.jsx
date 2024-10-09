3eeaa4a17695e87a16086f064ff02e9633b4ffcd
Jisung Lee <jskurt9352@gmail.com>
2024-10-09 15:55:54 +0900

    feat: add feature of Home page

--- src/components/emotional-diary/pages/home.jsx
+++ src/components/emotional-diary/pages/home.jsx
@@ -1,14 +1,24 @@
 import Header from '../elements/header/header'
 import Button from '../elements/button/button'
 import DiaryList from '../diary-list/diary-list'
+import { useState } from 'react'
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date())
+  const handleToDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1))
+  }
+
+  const handleToIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+  }
+
   return (
     <div>
       <Header
-        title={'Oct 2024'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
+        title={`${pivotDate.getMonth() + 1} ${pivotDate.getFullYear()}`}
+        leftChild={<Button text={'<'} onClick={handleToDecreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={handleToIncreaseMonth} />}
       />
       <DiaryList />
     </div>
